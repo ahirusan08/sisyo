1e261bfa75d86947846d8a8c6dac61cce10856bd
student <student@WIN-817G5PLIKEO>
2024-06-17 16:49:10 +0900

    0617 tao 1649

---
+++ src/main/java/com/example/demo/entity/Host.java
@@ -0,0 +1,62 @@
+package com.example.demo.entity;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+//taori
+
+@Entity
+@Table(name = "Hosts")
+@Data
+public class Host {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	private String name;
+	
+	private String password;
+	
+	@Column(name= "created_at")
+	private LocalDateTime createdAt;
+	
+	@Column(name= "created_by")
+	private Integer createdBy;
+	
+	@Column(name= "update_at")
+	private LocalDateTime updateAt;
+	
+	@Column(name= "update_by")
+	private Integer updateBy;
+	
+	@Column(name= "version_no")
+	private Integer versionNo;
+	
+	@Column(name= "delete_flag")
+	private Integer deleteFlag;
+	
+	public Host() {
+		
+	}
+
+	public Host(Integer id, String name, String password) {
+		this.id = id;
+		this.name = name;
+		this.password = password;
+	}
+
+	public Host(String name, String password) {
+		this.name = name;
+		this.password = password;
+	}
+	
+	
+
+}
