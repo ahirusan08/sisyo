9c54c57daaad6d054787fdb0c32601bac04158ef
student <student@WIN-QA12IFLCJ0P>
2024-06-18 09:59:19 +0900

    0618 kikusui 0959

--- src/main/java/com/example/demo/controller/UserAccountController.java
+++ src/main/java/com/example/demo/controller/UserAccountController.java
@@ -2,20 +2,31 @@
 
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.demo.model.UserAccount;
+import com.example.demo.repository.UserRepository;
+
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping("/user")
 public class UserAccountController {
 
-//	@Autowired
-//	private HttpSession session;
+	@Autowired
+	private HttpSession session;
 	
-//	@Autowired
-//  Account account;
+	@Autowired
+	UserRepository userRepository;
+	
+	@Autowired
+	UserAccount userAccount;
 	
 
 	@GetMapping({ "/index", "/logout" })
@@ -27,10 +38,28 @@ public class UserAccountController {
 	}
 	
 	@PostMapping("/login")
-	public String login(//@RequestParam(name="email" required=false) String email
+	public String login(
+			@RequestParam(name="email", required=false) String email,
+			@RequestParam(name="password", required=false) String password,
+			//菊：エラー文章表示用
+			Model m
 			) {
-		//エラー処理あり
 		
+		User user = null;
+		
+		//エラー処理あり
+		//菊：空欄処理
+		if (email == null || password == null) {
+			m.addAttribute("error", "メールアドレスまたはパスワードが空欄です");
+			return "userLogin";
+		}
+		
+		Optional<User> record = userRepository.findByEmailAndPassword(email, password);
+		if (record.isEmpty() == true) {
+			m.addAttribute("error", "メールアドレスまたはパスワードが空欄です");
+			return "userLogin";
+		}
+
 		
 		//正常時
 		//UserAccountクラスに名前をセット(セッション管理)
