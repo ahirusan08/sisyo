630cbfc328c0408e92e2795b66ab550b05885614
student <student@WIN-GFJMOVS5CQH>
2024-06-18 15:01:58 +0900

    0618 misaki 1501

--- src/main/java/com/example/demo/controller/BookController.java
+++ src/main/java/com/example/demo/controller/BookController.java
@@ -1,31 +1,58 @@
 package com.example.demo.controller;
 
+import java.time.LocalDate;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.demo.entity.Book;
+import com.example.demo.repository.BookRepository;
 
 @Controller
 public class BookController {
 
-//	@Autowired
-//	private HttpSession session;
+	//	@Autowired
+	//	private HttpSession session;
 
-	
+	@Autowired
+	BookRepository bookRepository;
 
 	@GetMapping("/show")
-	public String show() {
-	
+	public String show(Model m) {
+		List<Book> books = bookRepository.findAll();
+		m.addAttribute("books", books);
+
 		return "showBook";
 	}
 
 	@PostMapping("/search")
 	public String search(
-			//
-			) {
-	
+			@RequestParam(name = "ym", required = false) String ym,
+			Model m) {
+		
+		//検索データ保持
+		m.addAttribute("ym", ym);
+		
+		//全件検索
+		List<Book> books = bookRepository.findAll();
+		m.addAttribute("books", books);
+		
+		//その月は何日まで？
+		String day[]=ym.split("-");
+		LocalDate date = LocalDate.of(Integer.parseInt(day[0]), Integer.parseInt(day[1]), 1);
+		int maxDay = date.lengthOfMonth();
+		m.addAttribute("maxDay", maxDay);
+		
+		//貸出状況表示
+		List<Book> rentals=bookRepository.rental(ym+"%");
+		List<Book> limits=bookRepository.limit(ym+"%");
+		
 		return "searchBook";
 	}
-
-
 
 }
--- src/main/java/com/example/demo/repository/BookRepository.java
+++ src/main/java/com/example/demo/repository/BookRepository.java
@@ -1,11 +1,25 @@
 package com.example.demo.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.example.demo.entity.Book;
 
 @Repository
 public interface BookRepository extends JpaRepository<Book, Integer> {
+	
+	
+	@Query(
+			value="SELECT * FROM rentals WHERE rental_date LIKE ?",
+			nativeQuery=true)
+	List<Book> rental(String ym);
+	
+	@Query(
+			value="SELECT * FROM rentals WHERE limit_date LIKE ?",
+			nativeQuery=true)
+	List<Book> limit(String ym);
 
 }
