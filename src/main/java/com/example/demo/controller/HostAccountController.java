d178279b612f964d329d81b7d27bdfa3f574009f
student <student@WIN-817G5PLIKEO>
2024-06-18 10:00:51 +0900

    0618 tao 1000

--- src/main/java/com/example/demo/controller/HostAccountController.java
+++ src/main/java/com/example/demo/controller/HostAccountController.java
@@ -1,43 +1,42 @@
 //担当：たおちゃん
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping("/host")
 public class HostAccountController {
 
-//	@Autowired
-//	private HttpSession session;
-
-	
+	@Autowired
+	private HttpSession session;
 
 	@GetMapping({ "/index", "/logout" })
 	public String index() {
-	//セッション破棄、強制返却処理AOP？
+		//セッション破棄、強制返却処理AOP？
+		session.invalidate();
 		
+
 		return "hostLogin";
 	}
 
 	@GetMapping("/login")
 	public String login(
-			//@RequestParam(name="id" required=false) String id
-			
-			) {
+	//@RequestParam(name="id" required=false) String id
+
+	) {
 		//エラー処理あり
 		//aaa
-		
+
 		//正常時
-		
-		
+
 		//HostAccountクラスに名前をセット(セッション管理)
-		
+
 		return "select";//G202機能選択画面へ
 	}
-
-
-
 
 }
