67bfccdbf4520248011243e1402b3379445d9ca0
student <student@WIN-GFJMOVS5CQH>
2024-06-20 09:05:38 +0900

    0620 misaki 0905

--- src/main/java/com/example/demo/controller/BookController.java
+++ src/main/java/com/example/demo/controller/BookController.java
@@ -39,10 +39,15 @@ public class BookController {
 	public String search(
 			@RequestParam(name = "ym", required = false) String ym,
 			Model m) {
+		
+		//検索データなしのとき
+		if(ym==null || ym.equals("")) {
+			return "redirect:/show";
+		}
 
 		//検索データ保持
 		m.addAttribute("ym", ym);
-
+		
 		//全件検索
 		List<Book> books = bookRepository.findAll();
 		m.addAttribute("books", books);
@@ -75,9 +80,51 @@ public class BookController {
 
 		List<Rental> rentals = null;
 
-		//③貸出状況のセット
+//		//③貸出状況のセット
+//		for (int i = 0; i < allBooks; i++) {//すべての本に対して
+//			rentals = rentalRepository.findByBookId(i+1);//bookIdの一致するrentalsを検索
+//			for (int j = 0; j < 31; j++) {//すべての日程に対して
+//				for (Rental r : rentals) {//rentalsに対して
+//					
+//					//③-0	 まだ返却されていない場合
+//					if(r.getReturnDate()==null) {
+//						r.setReturnDate(r.getLimitDate());//返却日に返却期限日を仮set
+//					}
+//					//③-1	 月を跨がない予約(貸出日と返却期限日が今月)
+//					if (r.getRentalDate().getMonthValue() == month && r.getReturnDate().getMonthValue() == month
+//							&&r.getRentalDate().getYear() == year && r.getReturnDate().getYear() == year
+//							) {
+//						if (r.getRentalDate().getDayOfMonth() <= j+1 && r.getReturnDate().getDayOfMonth() >= j+1) {
+//							rentalBook[i][j] = 1;
+//						}
+//					}
+//
+//					//③-2	 月を跨ぐ予約
+//					//③-2-1 貸出日は今月だが返却日が来月のとき
+//					if (r.getRentalDate().getMonthValue() == month &&r.getReturnDate().getMonthValue() == month + 1
+//							&&r.getRentalDate().getYear() == year && r.getReturnDate().getYear() == year
+//							) {
+//						if (r.getRentalDate().getDayOfMonth() <= j+1) {
+//							rentalBook[i][j] = 1;
+//						}
+//					}
+//					//③-2-2 貸出日は先月だが返却日が今月のとき
+//					if (r.getRentalDate().getMonthValue() == month - 1 && r.getReturnDate().getMonthValue() == month
+//							&&r.getRentalDate().getYear() == year && r.getReturnDate().getYear() == year
+//							) {
+//						if (r.getReturnDate().getDayOfMonth() >= j+1) {
+//							rentalBook[i][j] = 1;
+//						}
+//					}
+//
+//				}
+//			}
+//		}
+//		
 		for (int i = 0; i < allBooks; i++) {//すべての本に対して
 			rentals = rentalRepository.findByBookId(i+1);//bookIdの一致するrentalsを検索
+			
+			
 			for (int j = 0; j < 31; j++) {//すべての日程に対して
 				for (Rental r : rentals) {//rentalsに対して
 					
@@ -86,7 +133,9 @@ public class BookController {
 						r.setReturnDate(r.getLimitDate());//返却日に返却期限日を仮set
 					}
 					//③-1	 月を跨がない予約(貸出日と返却期限日が今月)
-					if (r.getRentalDate().getMonthValue() == month && r.getReturnDate().getMonthValue() == month) {
+					if (r.getRentalDate().getMonthValue() == month && r.getReturnDate().getMonthValue() == month
+							&&r.getRentalDate().getYear() == year && r.getReturnDate().getYear() == year
+							) {
 						if (r.getRentalDate().getDayOfMonth() <= j+1 && r.getReturnDate().getDayOfMonth() >= j+1) {
 							rentalBook[i][j] = 1;
 						}
@@ -94,13 +143,17 @@ public class BookController {
 
 					//③-2	 月を跨ぐ予約
 					//③-2-1 貸出日は今月だが返却日が来月のとき
-					if (r.getRentalDate().getMonthValue() == month &&r.getReturnDate().getMonthValue() == month + 1) {
+					if (r.getRentalDate().getMonthValue() == month &&r.getReturnDate().getMonthValue() == month + 1
+							&&r.getRentalDate().getYear() == year && r.getReturnDate().getYear() == year
+							) {
 						if (r.getRentalDate().getDayOfMonth() <= j+1) {
 							rentalBook[i][j] = 1;
 						}
 					}
 					//③-2-2 貸出日は先月だが返却日が今月のとき
-					if (r.getRentalDate().getMonthValue() == month - 1 && r.getReturnDate().getMonthValue() == month) {
+					if (r.getRentalDate().getMonthValue() == month - 1 && r.getReturnDate().getMonthValue() == month
+							&&r.getRentalDate().getYear() == year && r.getReturnDate().getYear() == year
+							) {
 						if (r.getReturnDate().getDayOfMonth() >= j+1) {
 							rentalBook[i][j] = 1;
 						}
